f8c814622b91ea957445d1ff319ccceb57320ffe
Edward Slavov <e.slavov11@gmail.com>
2016-04-18 23:44:47 +0300

    pass change templ

--- app/password-change/password-change.js
+++ app/password-change/password-change.js
@@ -1,3 +1,29 @@
-/**
- * Created by Edi on 18-Apr-16.
- */
+'use strict';
+
+angular.module('issueTrackingSystem.password-change', [
+        'ngRoute',
+        'issueTrackingSystem.users.authentication'])
+
+    .controller('HomeController', [
+        '$scope',
+        '$window',
+        'authentication',
+        function($scope, $window , authentication) {
+            $scope.changePassword = function (changePasswordData) {
+                if (changePasswordData.newPassword !== changePasswordData.newPasswordConfirm) {
+                    // TODO: ERROR
+                    alert('Passwords do not match');
+                } else if (changePasswordData.oldPassword === changePasswordData.newPassword) {
+                    // TODO: ERROR
+                    alert('Old password cannot be new password... SHOULD I BE HANDLED BEFORE OR AFTER REST??');
+                }
+
+                authentication.changePassword(changePasswordData)
+                    .then(function (success) {
+                        $window.location.href = '/';
+                    }, function (error) {
+                        alert('Change password error ' + error);
+                        console.log(error);
+                    });
+            };
+        }]);
